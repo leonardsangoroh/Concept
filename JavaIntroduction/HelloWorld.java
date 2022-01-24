d89bddffa0739f198da3c4eb6b22176681cdcfa8
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-01-25 01:04:55 +0300

    Hello world program
    Intro to java

---
+++ JavaIntroduction/HelloWorld.java
@@ -0,0 +1,12 @@
+package JavaIntroduction;
+
+class HelloWorld {
+    public static void main(String[]args) {
+
+        System.out.println("Hello Leo");
+    }
+}
+
+
+// [javac 'file name'.java] - to compile code
+// [java 'file name'] - to run
