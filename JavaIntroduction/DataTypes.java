f8671b62eb46d07d31114a30c3d852ac56a354e6
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-01-25 23:12:12 +0300

    Data types

---
+++ JavaIntroduction/DataTypes.java
@@ -0,0 +1,46 @@
+package JavaIntroduction;
+
+public class DataTypes {
+    
+    //Variable - containers for storing data values
+
+    //Data types - specify type of data that can be stored in a variable
+
+    // True or false, default is false
+    boolean flag = true;
+
+    //-128 to 127 ( saves memory)
+    byte range = 124;
+
+    // -32768 to 32767
+    short temp = -200;
+
+    // both positive and negative values ( wider range)
+    int number = -425000;
+    
+    //long type is similar to int type
+
+    // float data type goes upto 4 decimal places
+    //Remember the 'f' at the end
+    float numberOne = 1.2345f;
+
+    //double doubles the float's capacity
+    double numberTwo = 1.23456789d;
+
+    //charater data type is for a single character
+    //Must be surrounded by single quotes
+    char gender = 'M';
+    
+    public static void main(String[]args) {
+
+        //String data type
+        String name = "Leonard Sangoroh";
+        int age = 20;
+        System.out.println(age);
+    }
+
+
+
+
+
+}
--- JavaIntroduction/HelloWorld.java
+++ JavaIntroduction/HelloWorld.java
@@ -3,7 +3,9 @@ package JavaIntroduction;
 class HelloWorld {
     public static void main(String[]args) {
 
+
         System.out.println("Hello Leo");
+        
     }
 }
 
