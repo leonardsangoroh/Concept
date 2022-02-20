0ff58e4a735e2266113f3929fb098d2f7aa33832
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2022-02-20 23:32:53 +0300

    Update Constructors

--- OOPjava/Constructors.java
+++ OOPjava/Constructors.java
@@ -2,4 +2,21 @@ package OOPjava;
 
 public class Constructors {
     //constructor - special method used to initialize objects
+
+    int modelYear;
+    String modelName;
+
+    //Contructor
+    public Constructors (int x, String y) {
+            modelYear = x;
+
+            modelName = y;
+    }
+
+    public static void main (String [] args) {
+
+        Constructors constOne = new Constructors(44, "Leo");
+
+        System.out.println("model year: " + constOne.modelYear + "\n" + "model name: " + constOne.modelName);
+    }
 }
